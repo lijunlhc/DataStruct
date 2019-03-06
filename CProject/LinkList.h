7bb7aa294d528837e1f118f7091964dc39eb4f84
lijunlhc <lijunlhc@gmail.com>
2019-03-06 13:52:05 +0800

    modified

--- CProject/LinkList.h
+++ CProject/LinkList.h
@@ -13,12 +13,18 @@
 #define OVERFLOW -2
 typedef int Status;
 typedef struct {
-
+	float coef;
+	int expn;
 } ElemType;
 typedef struct LNode {
 	ElemType data;
 	struct LNode *next;
 }LNode, *LinkList;
 Status InitList(LinkList L);
+Status GetElem(LinkList L, int i, ElemType *e);
+LNode *LocateElem(LinkList L, ElemType e);
+Status ListInsert(LinkList L, int i, ElemType e);
+Status ListDelete(LinkList L, int i);
+void CreateList_H(LinkList L, int n);
 
 #endif /* LINKLIST_H_ */
--- CProject/test.c
+++ CProject/test.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "LinkList.h"
 int main() {
-	LinkList list;
-	Status s = InitList(list);
+	LinkList list = (LNode *)malloc(sizeof(LNode));
+	CreateList_H(list, 2);
 	return 0;
 }
