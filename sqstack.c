462c6264516e33b09a3af72b7086954529ca29f5
lijunlhc <lijunlhc@foxmail.com>
2019-04-11 09:55:05 +0800

    modified

--- sqstack.c
+++ sqstack.c
@@ -1,13 +1,23 @@
-/*
- * stack_sq.c
- *
- *  Created on: 2019年3月9日
- *      Author: junli
- */
-
+#include <stdio.h>
 #include <stdlib.h>
 
-#include "sqstack.h"
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+typedef int Status;
+
+#define MAXSIZE 100
+
+typedef struct {
+	int num;
+} ElemType;
+
+typedef struct {
+	ElemType *base;
+	ElemType *top;
+	int stacksize;
+} SqStack;
+
 Status InitSqStack(SqStack *S) {
 	S->base = (ElemType *) malloc(sizeof(ElemType) * MAXSIZE);
 	if (!S->base)
@@ -16,6 +26,33 @@ Status InitSqStack(SqStack *S) {
 	S->stacksize = MAXSIZE;
 	return OK;
 }
+
+Status SqStackEmpty(SqStack S) {
+	if (S.top == S.base)
+		return 1;
+	else
+		return 0;
+}
+
+int StackLength(SqStack S) {
+	return S.top - S.base;
+}
+
+Status ClearStack(SqStack *S) {
+	if(S->base)
+		S->top = S->base;
+	return OK;
+}
+
+Status DestroyStack(SqStack *S) {
+	if(S->base) {
+		free(S->base);
+		S->stacksize = 0;
+		S->base = S->top = NULL;
+	}
+	return OK;
+}
+
 Status PushSqStack(SqStack *S, ElemType e) {
 	if (S->top - S->base == S->stacksize)
 		return ERROR;
@@ -29,14 +66,13 @@ Status PopSqStack(SqStack *S, ElemType *e) {
 	return OK;
 }
 Status GetTopSqStack(SqStack S, ElemType *e) {
-	if(S.top == S.base)
+	if (S.top == S.base)
 		return ERROR;
 	*e = *(S.top - 1);
 	return OK;
 }
-Status SqStackEmpty(SqStack S) {
-	if(S.top == S.base)
-		return OK;
-	else
-		return ERROR;
+
+int main() {
+	printf("测试\n");
+	return 0;
 }
