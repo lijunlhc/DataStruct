3059d5832e647fbb4d3e972a10c6c18f91fe0b89
lijunlhc <lijunlhc@foxmail.com>
2019-04-15 16:11:54 +0800

    modified

--- linkstack.c
+++ linkstack.c
@@ -5,22 +5,45 @@
  *      Author: junli
  */
 
+#include <stdio.h>
 #include <stdlib.h>
 
-#include "linkstack.h"
-Status InitLinkStack(LinkStack *S) {
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+typedef int Status;
+
+typedef struct {
+	char ch;
+} SElemType;
+
+typedef struct StackNode {
+	SElemType data;
+	struct StackNode *next;
+} StackNode, *LinkStack;
+
+Status InitStack(LinkStack *S) {
 	*S = NULL;
 	return OK;
 }
-Status PushLinkStack(LinkStack *S, SElemType e) {
-	StackNode *p = (StackNode *)malloc(sizeof(SElemType));
+
+Status StackEmpty(LinkStack S) {
+	if (S == NULL)
+		return 1;
+	else
+		return 0;
+}
+
+Status Push(LinkStack *S, SElemType e) {
+	StackNode *p = (StackNode *) malloc(sizeof(SElemType));
 	p->data = e;
 	p->next = *S;
 	*S = p;
 	return OK;
 }
-Status PopLinkStack(LinkStack *S, SElemType *e) {
-	if(*S == NULL)
+
+Status Pop(LinkStack *S, SElemType *e) {
+	if (*S == NULL)
 		return ERROR;
 	*e = (*S)->data;
 	StackNode *p = *S;
@@ -28,13 +51,22 @@ Status PopLinkStack(LinkStack *S, SElemType *e) {
 	free(p);
 	return OK;
 }
-SElemType GetTopLinkStack(LinkStack S) {
-	if(S != NULL)
+
+SElemType GetTop(LinkStack S) {
+	if (S == NULL)
+		exit(1);
+	else
 		return S->data;
 }
+
 Status LinkStackEmpty(LinkStack S) {
-	if(S != NULL)
+	if (S != NULL)
 		return ERROR;
 	else
 		return OK;
+}
+
+int main() {
+	printf("Hello World\n");
+	return 0;
 }
