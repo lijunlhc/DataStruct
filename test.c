5efb5c1be15332a51937b43ebf0d8d36bc040617
lijunlhc <lijunlhc@foxmail.com>
2019-04-16 11:29:38 +0800

    modified

---
+++ test.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+#define LEN 10
+
+void InsertSort(int a[], int length)
+{
+    int i, j;
+    for(i = 2; i <= length; ++i)
+    {
+        if(a[i] > a[i-1])
+        {
+            a[0] = a[i];
+            a[i] = a[i-1];
+            for(j = i - 2; a[0] > a[j]; --j)
+                a[j+1] = a[j];
+            a[j+1] = a[0];
+        }
+    }
+}
+
+int Partition(int a[], int low, int high)
+{
+    a[0] = a[low];
+    int pivotkey = a[low];
+    while(low < high)
+    {
+        while(low < high && a[high] <= pivotkey)
+            --high;
+        a[low] = a[high];
+        while(low < high && a[low] >= pivotkey)
+            ++low;
+        a[high] = a[low];
+    }
+    a[low] = a[0];
+//    printf("pivotkey = %d: ", pivotkey);
+//    for(int j = 1; j <= LEN; ++j)
+//    {
+//        printf("%d ", a[j]);
+//    }
+//    printf("\n");
+    return low;
+}
+void QSort(int a[], int low, int high)
+{
+    if(low < high)
+    {
+        int pivotkey = Partition(a, low, high);
+        QSort(a, low, pivotkey - 1);
+        QSort(a, pivotkey + 1, high);
+    }
+}
+void QuickSort(int a[], int length)
+{
+    QSort(a, 1, length);
+}
+
+void SelectSort(int a[], int length)
+{
+    for(int i = 1; i < length; ++i)
+    {
+        int k = i;
+        for(int j = i + 1; j <= length; ++j)
+            if(a[j] > a[k])
+                k = j;
+        int t;
+        if(k != i)
+        {
+            t = a[i];
+            a[i] = a[k];
+            a[k] = t;
+        }
+    }
+}
+
+int main()
+{
+    int a[LEN+1] = {0, 12, 2, 16, 30, 28, 10, 16, 20, 6, 18};
+    int i;
+    printf("原始序列: ");
+    for(i = 1; i <= LEN; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+    //InsertSort(a, LEN);
+    //QuickSort(a, LEN);
+    SelectSort(a, LEN);
+    printf("排序序列: ");
+    for(i = 1; i <= LEN; i++)
+        printf("%d ", a[i]);
+    printf("\n");
+    return 0;
+}
