702fae0b4165d00c1c501d49af905b09e26b3837
lijunlhc <lijunlhc@foxmail.com>
2019-05-05 07:34:40 +0800

    m

--- huffmantree.c
+++ huffmantree.c
@@ -3,10 +3,10 @@
 
 #define OK 1
 #define ERROR 0
+#define OVERFLOW -2
 typedef int Status;
 
-typedef struct
-{
+typedef struct {
     int weight;
     int parent, lchild, rchild;
 } HTNode, *HuffmanTree;
